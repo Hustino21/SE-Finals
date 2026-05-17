87051e7e0c4aae2b84a7be834ad33c5c25fcb767
Hustino21 <reyes.justin2121@gmail.com>
2026-05-18 01:06:46 +0800

    Delete music_label directory

--- music_label/includes/footer.php
+++
@@ -1,5 +0,0 @@
-</div> <div style="text-align: center; margin-top: 20px; color: var(--text-secondary); font-size: 0.8rem;">
-        &copy; <?= date('Y') ?> Music Label Management System. All rights reserved.
-    </div>
-</body>
-</html>
--- music_label/includes/logger.php
+++
@@ -1,6 +0,0 @@
-<?php
-function logActivity($pdo, $username, $action_type, $details) {
-    $stmt = $pdo->prepare("INSERT INTO activity_logs (username, action_type, details) VALUES (?, ?, ?)");
-    $stmt->execute([$username, $action_type, $details]);
-}
-?>
